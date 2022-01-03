08300b3dc919e3476565f741af7e98a51526d04d
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-03 21:13:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+
+
 copy("https://raw.githubusercontent.com/bedjan/w1/main/obsah.php", "obsah.php");
 
-include('obsah.php');
+include("obsah.php");
 
-include('mdwiki.html#!index.md');
+
 ?>
