3f1fc72c77b0354e06abf30f30520883539d5f4a
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-03 21:18:21 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,6 +1,4 @@
 <?php
-copy("https://raw.githubusercontent.com/bedjan/w/main/navigation.md", "navigation.md");
-copy("https://raw.githubusercontent.com/bedjan/w/main/index.md", "index.md");
 copy("https://raw.githubusercontent.com/bedjan/w/main/multimedia.md", "multimedia.md");
 copy("https://raw.githubusercontent.com/bedjan/w/main/linux.md", "linux.md");
 copy("https://raw.githubusercontent.com/bedjan/w/main/linux-cli.md", "linux-cli.md");
