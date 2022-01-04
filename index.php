836316044fd6bad0200b7f60e292e85db74bbf4d
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-04 23:34:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $homepage = file_get_contents('http://cizinou.sweb.cz/mdwiki.html#index.md');
 echo $homepage;
 copy("https://raw.githubusercontent.com/bedjan/w1/main/obsah.php", "obsah.php");
-
+sleep(10);
 include("obsah.php");
 ?>
