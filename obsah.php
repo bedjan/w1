c892ab8594811bcd32d88da80a6f5de638a6eab2
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-04 23:52:10 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,6 +1,9 @@
 <?php
-copy("https://raw.githubusercontent.com/bedjan/w/main/multimedia.md", "multimedia.md");
-copy("https://raw.githubusercontent.com/bedjan/w/main/linux.md", "linux.md");
-copy("https://raw.githubusercontent.com/bedjan/w/main/linux-cli.md", "linux-cli.md");
-copy("https://raw.githubusercontent.com/bedjan/w/main/ekologie.md", "ekologie.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/web.md", "index.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/navigation.md", "navigation.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/multimedia.md", "multimedia.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux.md", "linux.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux-cli.md", "linux-cli.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/ekologie.md", "ekologie.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/polstina.md", "polstina.md");
 ?>
