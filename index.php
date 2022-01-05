12c80fd4f70f8434d8bb36d81da188e1bbf31b69
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-05 21:27:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+$filename = 'obsah.php';
+
+if (file_exists($filename)) {
+copy("https://raw.githubusercontent.com/bedjan/w1/main/obsah.php", "obsah.php");
+include('obsah.php');
+} else {
+copy("https://raw.githubusercontent.com/bedjan/w1/main/obsah.php", "obsah.php");
+include('obsah.php');
+}
+
 $homepage = file_get_contents('http://cizinou.sweb.cz/mdwiki.html#index.md');
 echo $homepage;
-copy("https://raw.githubusercontent.com/bedjan/w1/main/obsah.php", "obsah.php");
-sleep(10);
-include("obsah.php");
 ?>
