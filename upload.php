dc4812820c9502e24f0ad58aeb56a27b4a0c8ae7
bedjan <29821749+bedjan@users.noreply.github.com>
2022-01-06 11:20:09 +0100

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,39 @@
+<?php
+copy("https://raw.githubusercontent.com/bedjan/w1/main/mdwiki.html", "mdwiki.html");
+copy("https://raw.githubusercontent.com/bedjan/web/main/navigation.md", "navigation.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/web.md", "index.md");
+
+copy("https://raw.githubusercontent.com/bedjan/web/main/mm.md", "mm.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux.md", "linux.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/linux-cli.md", "linux-cli.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/ekologie.md", "ekologie.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/polstina.md", "polstina.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/php.md", "php.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/ekologie.md", "ekologie.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/vyziva.md", "vyziva.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/lecba.md", "lecba.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/germanismy.md", "germanismy.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/git.md", "git.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/markdown.md", "markdown.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/hooponopono.md", "hooponopono.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/knihy.md", "knihy.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/vadim-zeland.md", "vadim-zeland.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/carnegie.md", "carnegie.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/systemd.md", "systemd.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/rec.md", "rec.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/manipulace.md", "manipulace.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/vyziva.md", "vyziva.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/lecba.md", "lecba.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/sociologie.md", "sociologie.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/psychosomatika.md", "psychosomatika.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/jak_poloha_akne_prozradi_co_mate_v_tele_nemocneho.md", "jak_poloha_akne_prozradi_co_mate_v_tele_nemocneho.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/hooponopono.md", "hooponopono.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/vadim-zeland.md", "vadim-zeland.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/predpony.md", "predpony.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/zivotopis.md", "zivotopis.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/radia.md", "radia.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/nemcina.md", "nemcina.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/nemecko.md", "nemecko.md");
+copy("https://raw.githubusercontent.com/bedjan/web/main/nemecko_gramatika.md", "nemecko_gramatika.md");
+
+?>
